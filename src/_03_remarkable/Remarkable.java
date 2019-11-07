6f4f33be68f42aec4cf17db694eae4c3f2513017
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-07 13:27:54 -0800

    Mustaches

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,11 +8,20 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+          String khanh = "Smart";
+          String diego = "Tactical";
+          String rosendo = "Kind Hearted";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+  String userName = JOptionPane.showInputDialog(null, "What is your name?");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
-	}
+        if(userName.equalsIgnoreCase("khanh")) {
+        	JOptionPane.showMessageDialog(null, "You're highly smart");
+        }
+        if(userName.equalsIgnoreCase("diego")) {
+        	JOptionPane.showMessageDialog(null, "You're very good at robeats and your tactical.");
+}       
+        if(userName.equalsIgnoreCase("rosendo")) {
+	JOptionPane.showMessageDialog(null, "You're very kind hearted.");
 }
-
+}
+}

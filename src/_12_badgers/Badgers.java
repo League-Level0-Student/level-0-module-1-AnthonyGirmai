99f532ccad73a44a6e18209f8044449ab893bb2e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-12 13:28:50 -0800

    My Age And Badger lyrics

---
+++ src/_12_badgers/Badgers.java
@@ -0,0 +1,25 @@
+package _12_badgers;
+public class Badgers {
+public static void main(String[] args) {
+	for(int lyrics = 0; lyrics < 12; lyrics ++)
+		System.out.print("Badger, ");
+	System.out.println();
+	for(int lyrics = 0; lyrics < 2; lyrics ++)
+	System.out.print("Mushroom, ");
+	for(int lyrics = 0; lyrics < 11; lyrics ++)
+	System.out.print("");
+	System.out.print("Badger, ");
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+}
+}
